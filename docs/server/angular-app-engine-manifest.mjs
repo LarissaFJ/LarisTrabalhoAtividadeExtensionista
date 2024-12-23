fc7e03ac4fd9b8825a8f18bd5418af12abdf8aef
Larissa Ferreira de Jesus <larissafj@gmail.com>
2024-12-23 11:23:11 -0300

    teste

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/LarisTrabalhoAtividadeExtensionista/',
+  basePath: '/',
   entryPoints: {
     '': () => import('./main.server.mjs')
   },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,18 +2,18 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/LarisTrabalhoAtividadeExtensionista/',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/LarisTrabalhoAtividadeExtensionista"
+    "route": "/"
   }
 ],
   assets: {
-    'index.csr.html': {size: 686, hash: 'd3def484affe32dab01c64c94eedd1250e26ecdf4625797d78e155e42faeed44', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1200, hash: '3f88c62860ea04c26706fddf804c33222112bd846d63602663a1f55fe8d3824d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 846, hash: '2343e3b3e933b8cc43576c20b77ba3bc8035b2f7ec2eb2cd186973dbe005df34', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 650, hash: '5845be01a4ca6aae17d5869c6342c4d66b4445991e15ee3955aca475efdf6473', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1164, hash: '2b510a5fbba1e8ed60ece07ab20b6efe55e875ec0bd021999d4eedfb1f479242', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 810, hash: '748f5bc507368ebbf1f12cf9e566af3f60f1a0290015820e103a6844395845cf', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
